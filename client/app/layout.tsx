1def7691d9fcae5241355123b755d0563fa3389d
Lolodusiji <sholaodusiji@gmail.com>
2023-11-13 13:12:08 +0100

    package update

--- client/app/layout.tsx
+++ client/app/layout.tsx
@@ -1,7 +1,6 @@
 import type { Metadata } from "next";
 // import { Inter } from 'next/font/google'
 import localFont from "next/font/local";
-// import { ThemeProvider } from "next-themes ";
 import "./globals.css";
 
 const myFont = localFont({
