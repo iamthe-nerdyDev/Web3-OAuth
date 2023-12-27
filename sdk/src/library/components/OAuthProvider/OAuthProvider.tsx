f370c8bbfc185134d44231f0fa59f46bf961cd5f
Adedeji Morifeoluwa <141498118+iamthe-nerdyDev@users.noreply.github.com>
2023-12-27 16:00:36 +0100

    fixed

--- sdk/src/library/components/OAuthProvider/OAuthProvider.tsx
+++ sdk/src/library/components/OAuthProvider/OAuthProvider.tsx
@@ -8,7 +8,36 @@ import {
   walletConnect,
 } from "@thirdweb-dev/react";
 import { IWeb3OAuthProvider } from "./OAuthProvider.types";
-import customNetwork from "../custom-network.json";
+
+const customNetwork = {
+  chainId: 201022,
+  networkId: 201022,
+  explorers: [
+    {
+      name: "fonscan",
+      url: "https://fonscan.io/",
+      standard: "EIP3091",
+    },
+  ],
+  icon: {
+    url: "https://iili.io/JRcuTDF.png",
+    width: 36,
+    height: 36,
+    format: "png",
+  },
+  infoURL: "https://fonchain.io",
+  rpc: ["https://fsc-dataseed1.fonscan.io", "https://fsc-dataseed2.fonscan.io"],
+  nativeCurrency: {
+    decimals: 18,
+    name: "FON",
+    symbol: "FON",
+  },
+  shortName: "FSC",
+  slug: "fon-smart-chain",
+  testnet: false,
+  chain: "FON",
+  name: "FON Smart Chain",
+};
 
 const OAuthProvider: React.FC<IWeb3OAuthProvider> = (props) => {
   return (
